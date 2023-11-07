525aed8d097796ee009888b454b411c907a4fc19
Rahul Parmar <85381931+Rawshawak@users.noreply.github.com>
2023-11-07 13:18:40 +0530

    Update

---
+++ web scrapper.py
@@ -0,0 +1,19 @@
+import requests
+from bs4 import BeautifulSoup
+
+# Define the URL you want to scrape
+url = 'https://instagram.com'
+
+# Send an HTTP GET request to the URL
+response = requests.get(url)
+
+# Parse the HTML content of the page
+soup = BeautifulSoup(response.text, 'html.parser')
+
+# Extract data from the page
+# For example, let's extract all the links on the page
+links = soup.find_all('a')
+
+# Print the links
+for link in links:
+    print(link.get('href'))
